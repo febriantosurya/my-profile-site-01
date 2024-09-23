784b45e02a3ffdc883b5a165467d0c068a988ce3
febriantosurya <febriantosurya5@gmail.com>
2024-09-23 12:42:29 +0700

    fix import

--- .github/actions/deploy-to-s3/main.js
+++ .github/actions/deploy-to-s3/main.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
+import core from "@actions/core";
+import exec from "@actions/exec;"
 
 function run() {
     const bucket = core.getInput('bucket', { required: true });
@@ -9,3 +9,5 @@ function run() {
     const s3uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${dist} ${s3uri} --region ${region}`);
 }
+
+run()
