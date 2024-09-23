22e67a526c2723f9aa2efc7b097a67615666a839
febriantosurya <febriantosurya5@gmail.com>
2024-09-23 11:05:07 +0700

    Add workflow and action to deploy to s3

---
+++ .github/actions/deploy-to-s3/main.js
@@ -0,0 +1,11 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+function run() {
+    const bucket = core.getInput('bucket', { required: true });
+    const region = core.getInput('region', { required: true });
+    const dist = core.getInput('dist', { required: true });
+
+    const s3uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${dist} ${s3uri} --region ${region}`);
+}
